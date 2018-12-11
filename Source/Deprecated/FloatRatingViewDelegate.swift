2e04ee34f8bf5891b5b01b43a242308d99a466f3
Vasily Ulianov <vasily@me.com>
2018-12-11 19:09:46 +0300

    Remove class because it is not needeed in obj-c

--- Source/Deprecated/FloatRatingViewDelegate.swift
+++ Source/Deprecated/FloatRatingViewDelegate.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 @available(*, deprecated, message: "Use the Target-Action design pattern from UIControl")
-@objc public protocol FloatRatingViewDelegate: class {
+@objc public protocol FloatRatingViewDelegate {
 	/// Returns the rating value when touch events end
 	@available(*, deprecated, message: "Use `addTarget(target:action:for:)` or Storyboard's valueChanged connection")
 	@objc optional func floatRatingView(_ ratingView: FloatRatingView, didUpdate rating: Double)
@@ -10,3 +10,4 @@ import Foundation
 	@available(*, deprecated, message: "Use `addTarget(target:action:for:)` or Storyboard's valueChanged connection and set `isContinuous` to `true`")
 	@objc optional func floatRatingView(_ ratingView: FloatRatingView, isUpdating rating: Double)
 }
+
